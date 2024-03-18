c33778c9a635b8a34a3cef4a14c18d7ee9ec7060
Prakhar <prakhar6601@gmail.com>
2024-03-18 20:25:58 +0530

    Adding new files

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const app=express();
+const PORT=3000||process.env.PORT;
+const rootRouter=require("./routes/index");
+
+app.use("/api/v1",rootRouter);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
 
 
